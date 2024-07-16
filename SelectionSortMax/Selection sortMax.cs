da5a1952431ef9874634367fae9841ff3a9676e6
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-17 09:44:51 +1100

    Создал программу сортировки методом выбора от больгого числа к малому

---
+++ SelectionSortMax/Selection sortMax.cs
@@ -0,0 +1,34 @@
+﻿int[] array = { 1, 2, 9, 4, 6, 4, 2, 15, 247 };
+
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    { 
+        Console.Write($"{mass[i]}, ");
+    }
+}
+
+void SelectionSortInMax(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        int posMax = i;
+
+        for (int j = i + 1; j < mass.Length; j++)
+        { 
+            if (mass[j] > mass[posMax]) posMax = j;
+        }
+
+        int temp = mass[i];
+        mass[i] = mass[posMax];
+        mass[posMax] = temp;
+    }
+
+}
+
+PrintArray(array);
+
+SelectionSortInMax(array);
+Console.WriteLine();
+
+PrintArray(array);
